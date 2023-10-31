c4d77a6b36f2babbaec07650f779235d7d2fb816
Raima Aftab <raima.aftab2022@gmail.com>
2023-10-31 18:37:22 +0500

    change nav
    nav bar with menu on small screen

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-// function to show side bar
-function showSidebar()
-{
-    // here we only want to make visible sidebar
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display='flex';
-}
-// hide side bar on cross click
-function hideSidebar()
-{
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display='none';  
-}
+const header = document.querySelector("header");
+      const menuToggler = document.querySelectorAll(".menu_toggle");
+      const body = document.body;
+
+      menuToggler.forEach(toggler => {
+        toggler.addEventListener("click", () => header.classList.toggle("showMenu"));
+        
+      });
+
+
+// const header = document.querySelector("header");
+// const menuToggler = document.querySelectorAll(".menu_toggle");
+// const body = document.body;
+
+// menuToggler.forEach(toggler => {
+//   toggler.addEventListener("click", () => {
+//     header.classList.toggle("showMenu");
+//     // to give blur effect on bar click btn
+//     body.classList.toggle("menu-open");
+//   });
+// });
