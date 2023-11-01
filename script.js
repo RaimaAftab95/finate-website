e19b4c85859a682c749e9daadbb67aea9c34c4b8
Raima Aftab <raima.aftab2022@gmail.com>
2023-11-01 09:07:24 +0500

    applying bootstrap5
    include bootstrap5 and sorting ui issues

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const header = document.querySelector("header");
         
       });
 
-
+// *********to give blur back groung on menue btn  click
 // const header = document.querySelector("header");
 // const menuToggler = document.querySelectorAll(".menu_toggle");
 // const body = document.body;
