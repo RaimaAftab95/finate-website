c4e9d3d6f8bfabc7ec68f066a682d4d2761658cd
Raima Aftab <raima.aftab2022@gmail.com>
2023-11-15 10:14:03 +0500

    home done
    home page complete with scroll to top btn

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ const header = document.querySelector("header");
 //     body.classList.toggle("menu-open");
 //   });
 // });
+
+// scroll to top
+// Get the scroll-to-top element
+const scrollToTop = document.getElementById('scroll-to-top');
+
+// Get the section2 element
+const section2 = document.getElementById('section#2');
+
+// Check if the scroll position is below the section1
+window.addEventListener('scroll', function () {
+  const section1OffsetTop = section2.offsetTop;
+  const scrollPosition = window.scrollY + window.innerHeight;
+
+  // Display the scroll-to-top icon only if the scroll position is below section1
+  if (scrollPosition >= section1OffsetTop) {
+    scrollToTop.style.display = 'block';
+  } else {
+    scrollToTop.style.display = 'none';
+  }
+});
